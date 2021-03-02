bad7e72d70bd63810ea968ddc266b0651b3db0ca
Justin Lavoie <justin.lavoie@mail.com>
2021-03-02 18:43:53 -0500

    made predictions more readable in terminal results

--- predict.py
+++ predict.py
@@ -3,50 +3,42 @@ from sqlalchemy import create_engine
 from surprise import Dataset, Reader
 from surprise import SVD, NMF
 from surprise.model_selection import cross_validate, train_test_split
-from numpy import NaN
+from numpy import NaN, isnan
 
-print("Connecting to db and querying ratings data...")
+print("Connecting to db and loading data into memory...")
 alchemyEngine = create_engine(
     'postgresql+psycopg2://postgres:@127.0.0.1/rec_system');
 dbConnection = alchemyEngine.connect();
-df = pd.read_sql("select id, isbn, rating from \"ratings\"", dbConnection);
-df_books = pd.read_sql("select isbn, title from \"books\"", dbConnection);
+ratings_df = pd.read_sql("select id, isbn, rating from \"ratings\"", dbConnection);
+books_df = pd.read_sql("select isbn, title, author, year from \"books\"", dbConnection);
 dbConnection.close();
 
-print("Loading data into memory...")
 reader = Reader(rating_scale=(1, 10))
-data = Dataset.load_from_df(df, reader)
+data = Dataset.load_from_df(ratings_df, reader)
 
 print("""
     Number of ratings: %d\n
     Number of books: %d\n
-    Number of users: %d""" % (len(df), 
-        len(df['isbn'].unique()), 
-        len(df['id'].unique())))
-
-
-print("SVD: crossvalidating...")
-model_svd = SVD()
-cv_results_svd = cross_validate(model_svd, data, cv=3)
-pd.DataFrame(cv_results_svd).mean()
+    Number of users: %d""" % (len(ratings_df), 
+        len(ratings_df['isbn'].unique()), 
+        len(ratings_df['id'].unique())))
 
 print("Fitting model...")
 model = SVD(n_factors=80, n_epochs=20, lr_all=0.005, reg_all=0.2)
+# model = NMF()
 model.fit(data.build_full_trainset())
 
-print("getting test predictions")
+print("Getting test predictions")
 
 selected_id = 500016
-predictions = [(x, model.predict(selected_id, x).est) for x in df['isbn'].unique()]
+predictions = [(x, model.predict(selected_id, x).est) for x in ratings_df['isbn'].unique()]
 predictions_df = pd.DataFrame(predictions, 
     columns=['isbn', 'rating']).sort_values(by="rating", 
-    ascending=False).merge(df_books, on='isbn', how='left')    
-print(predictions_df)
+    ascending=False).merge(books_df, on='isbn', how='left')    
 
-
-
-
-
-
-
-
+for i, row, in predictions_df.head(10).iterrows():
+    print("{}: {}".format(i + 1, row.title), end="")
+    if ~isnan(row.year):
+        print(" ({})".format(int(row.year)), end="")
+    print("\nby {}".format(row.author))
+    print("{}\n".format(row.rating))
