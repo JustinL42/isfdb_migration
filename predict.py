14805187f6d862f97bf670f30e7ac0dc96477b03
Justin Lavoie <justin.lavoie@mail.com>
2021-03-06 19:18:37 -0500

    added cross validation

--- predict.py
+++ predict.py
@@ -1,8 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
-from surprise import Dataset, Reader
-from surprise import SVD, NMF
-from surprise.model_selection import cross_validate, train_test_split
+from surprise import Dataset, Reader, SVD, NMF, KNNBasic, accuracy
+from surprise.model_selection import cross_validate, train_test_split, GridSearchCV
 from numpy import NaN, isnan
 
 print("Connecting to db and loading data into memory...")
@@ -23,15 +22,17 @@ print("""
         len(ratings_df['isbn'].unique()), 
         len(ratings_df['id'].unique())))
 
-print("Fitting model...")
-model = SVD(n_factors=80, n_epochs=20, lr_all=0.005, reg_all=0.2)
-# model = NMF()
-model.fit(data.build_full_trainset())
+algo = SVD(n_factors=80, n_epochs=20, lr_all=0.005, reg_all=0.2)
+# algo = KNNBasic()
+# algo = NMF()
+print("algo: {}".format(algo))
+print("Cross validating...")
+cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=3, verbose=True)
 
 print("Getting test predictions")
-
-selected_id = 500016
-predictions = [(x, model.predict(selected_id, x).est) for x in ratings_df['isbn'].unique()]
+selected_user = str(500016)
+predictions = [(isbn, algo.predict(selected_user, isbn).est) \
+                for isbn in ratings_df['isbn'].unique()]
 predictions_df = pd.DataFrame(predictions, 
     columns=['isbn', 'rating']).sort_values(by="rating", 
     ascending=False).merge(books_df, on='isbn', how='left')    
@@ -41,4 +42,5 @@ for i, row, in predictions_df.head(10).iterrows():
     if ~isnan(row.year):
         print(" ({})".format(int(row.year)), end="")
     print("\nby {}".format(row.author))
+    print(row.isbn)
     print("{}\n".format(row.rating))
