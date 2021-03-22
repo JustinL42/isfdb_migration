5c7b91c1ab5d042d2a123d0fafa49b9fa9fe9b51
Justin Lavoie <justin.lavoie@mail.com>
2021-03-22 18:16:31 -0400

    refactored predict script

--- predict.py
+++ predict.py
@@ -1,57 +1,64 @@
 import pandas as pd
 from sqlalchemy import create_engine
-from surprise import Dataset, Reader, SVD, NMF, KNNBasic, accuracy
-from surprise.model_selection import cross_validate, train_test_split, GridSearchCV
-from numpy import NaN, isnan
+from surprise import Dataset, Reader, SVD, KNNBasic, accuracy
+from numpy import isnan
 
 print("Connecting to db and loading data into memory...")
 alchemyEngine = create_engine(
     'postgresql+psycopg2://postgres:@127.0.0.1/rec_system');
-dbConnection = alchemyEngine.connect();
-ratings_df = pd.read_sql("select id, isbn, rating from \"ratings\"", dbConnection);
-books_df = pd.read_sql("select isbn, title, author, year from \"books\"", dbConnection);
-dbConnection.close();
+conn = alchemyEngine.connect();
+ratings_df = pd.read_sql("""
+    SELECT id, isbn, rating 
+    FROM "ratings"
+    """, conn);
+books_df = pd.read_sql("""
+    SELECT isbn, title, author, year 
+    FROM "books"
+    """, conn);
+conn.close();
 
 reader = Reader(rating_scale=(1, 10))
 data = Dataset.load_from_df(ratings_df, reader)
 train_set = data.build_full_trainset()
 
 print("""
-    Number of ratings: %d\n
-    Number of books: %d\n
-    Number of users: %d""" % (len(ratings_df), 
+    Number of ratings: {}\n
+    Number of books: {}\n
+    Number of users: {}\n""".format(
+        len(ratings_df), 
         len(ratings_df['isbn'].unique()), 
         len(ratings_df['id'].unique())))
 
-algo = SVD(biased=False, random_state=777, verbose=True)
-# algo = KNNBasic(verbose=True)
+# algo = SVD(biased=True, random_state=777, verbose=True)
+algo = KNNBasic(verbose=True)
 print("algo: {}".format(algo))
 print("Fitting train_set")
 algo.fit(train_set)
 
 
 print("Getting test predictions")
-# selected_user = 171118
-selected_user = 500016
+# selected_user = 500016
+selected_user = 500000
 predictions = [(isbn, algo.predict(selected_user, isbn).est) \
                 for isbn in ratings_df['isbn'].unique()]
 predictions_df = pd.DataFrame(predictions, 
     columns=['isbn', 'rating']).sort_values(by="rating", 
-    ascending=False).merge(books_df, on='isbn', how='left')    
+    ascending=False).merge(books_df, on='isbn', how='left')
 
-for i, row, in predictions_df.head(40).iterrows():
+
+def print_row(row):
     print("{}: {}".format(i + 1, row.title), end="")
     if ~isnan(row.year):
         print(" ({})".format(int(row.year)), end="")
-    print("\nby {}".format(row.author))
-    print(row.isbn)
-    print("{}\n".format(row.rating))
+    print("\nby {}   {}   {}\n".format(row.author, 
+        row.isbn, round(row.rating, 2)))
 
+top_x = 20
+print("\nTop {}:".format(top_x))
+for i, row, in predictions_df.head(top_x).iterrows():
+    print_row(row)
 
-for i, row, in predictions_df.tail(20).iterrows():
-    print("{}: {}".format(i + 1, row.title), end="")
-    if ~isnan(row.year):
-        print(" ({})".format(int(row.year)), end="")
-    print("\nby {}".format(row.author))
-    print(row.isbn)
-    print("{}\n".format(row.rating))
+bottom_x = 5
+print("\nBottom {}:".format(bottom_x))
+for i, row, in predictions_df.tail(bottom_x).iterrows():
+    print_row(row)
