063cc6f2d3f14f3c75528bee6e6f433568f546a0
Justin Lavoie <justin.lavoie@mail.com>
2021-03-24 01:54:38 -0400

    moved isbnSet creation to util.py

--- etl.py
+++ etl.py
@@ -2,8 +2,7 @@
 import pandas as pd
 import psycopg2
 from psycopg2.extras import execute_values
-import mysql.connector
-import pickle
+import util
 
 db_conn_string = "dbname=rec_system user=postgres"
 
@@ -51,43 +50,7 @@ os.system("""
 	tr -d '\\\"' > utf-ratings.csv""")
 
 print("Getting set of speculative fiction ISBNs from ISFDB...")
-# Use the pickled copy if it exists. Otherwise, get it 
-# from the db, turn it into a Set and pickle it for next time
-try:
-	isbnSet = pickle.load(open("isbnSet.pickle", "rb"))
-except FileNotFoundError: 
-	mysql_conn = mysql.connector.connect(
-		host="localhost",
-		user="root",
-		password="",
-		database="isfdb"
-	)
-
-	mysql_cur = mysql_conn.cursor()
-
-	# Get all the pulication ISBNs associated with titles 
-	# that aren't graphic or non-genre
-	mysql_cur.execute("""
-	SELECT DISTINCT pub_isbn
-	FROM pubs
-	WHERE pub_id in (
-		SELECT pub_id 
-		FROM pub_content
-		WHERE title_id in (
-			SELECT title_id
-			FROM titles
-			WHERE title_non_genre = 'No'
-			AND title_graphic = 'No'
-		) 
-	) 
-	AND pub_isbn is not NULL
-	AND pub_isbn != ''
-	AND LENGTH(pub_isbn) <= 13;""")
-
-	isbnSet = set([str(x[0]) for x in mysql_cur.fetchall()])
-	pickle.dump(isbnSet, open("isbnSet.pickle", "wb"))
-	mysql_cur.close()
-	mysql_conn.close()
+isbnSet = util.get_isbn_set()
 
 
 #create database
--- predict.py
+++ predict.py
@@ -5,23 +5,23 @@ from numpy import isnan
 
 print("Connecting to db and loading data into memory...")
 alchemyEngine = create_engine(
-    'postgresql+psycopg2://postgres:@127.0.0.1/rec_system');
-conn = alchemyEngine.connect();
+    'postgresql+psycopg2://postgres:@127.0.0.1/rec_system')
+conn = alchemyEngine.connect()
 ratings_df = pd.read_sql("""
     SELECT id, isbn, rating 
     FROM "ratings"
-    """, conn);
+    """, conn)
 books_df = pd.read_sql("""
     SELECT isbn, title, author, year 
     FROM "books"
-    """, conn);
-conn.close();
+    """, conn)
+conn.close()
 
 reader = Reader(rating_scale=(1, 10))
 data = Dataset.load_from_df(ratings_df, reader)
 train_set = data.build_full_trainset()
 
-print("""
+print("""   
     Number of ratings: {}\n
     Number of books: {}\n
     Number of users: {}\n""".format(
@@ -29,19 +29,32 @@ print("""
         len(ratings_df['isbn'].unique()), 
         len(ratings_df['id'].unique())))
 
-params = {'lr_all': 0.012, 'n_epochs': 30, 'reg_all': 0.2}
+param_grid = {
+    'random_state': 777,
+    'biased': True, 
+    'n_factors': 20,
+    'n_epochs': 30, 
+    'lr_bu': 0.012,
+    'lr_bi': 0.005,
+    'lr_pu': 0.005,
+    'lr_qi': 0.005,  
+    'reg_bu': 0.1,
+    'reg_bi': 0.3,
+    'reg_pu': 0.3,
+    'reg_qi': 0.3
+    }
 
-algo = SVD(lr_all=0.012, n_epochs=30, reg_all=0.2, 
-    random_state=777, verbose=True)
+algo = SVD(**param_grid)
 # algo = KNNBasic(verbose=True)
-print("algo: {}".format(algo))
+print("algo: {}".format(algo.__class__.__name__))
 print("Fitting train_set")
 algo.fit(train_set)
 
 
 print("Getting test predictions")
 # selected_user = 500016
-selected_user = 500000
+# selected_user = 500000
+selected_user = 600000
 predictions = [(isbn, algo.predict(selected_user, isbn).est) \
                 for isbn in ratings_df['isbn'].unique()]
 predictions_df = pd.DataFrame(predictions, 
@@ -49,7 +62,7 @@ predictions_df = pd.DataFrame(predictions,
     ascending=False).merge(books_df, on='isbn', how='left')
 
 
-def print_row(row):
+def print_row(i, row):
     print("{}: {}".format(i + 1, row.title), end="")
     if ~isnan(row.year):
         print(" ({})".format(int(row.year)), end="")
@@ -58,10 +71,10 @@ def print_row(row):
 
 top_x = 20
 print("\nTop {}:".format(top_x))
-for i, row, in predictions_df.head(top_x).iterrows():
-    print_row(row)
+for i, row in predictions_df.head(top_x).iterrows():
+    print_row(i, row)
 
 bottom_x = 5
 print("\nBottom {}:".format(bottom_x))
-for i, row, in predictions_df.tail(bottom_x).iterrows():
-    print_row(row)
+for i, row in predictions_df.tail(bottom_x).iterrows():
+    print_row(i, row)
