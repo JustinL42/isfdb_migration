77916c82bec1676115d8be77326cdd0a01407453
Justin Lavoie <justin.lavoie@mail.com>
2021-04-18 21:40:41 -0400

    Finished essential datagathering logic for migration script

--- book_db_migration/migration_script.py
+++ book_db_migration/migration_script.py
@@ -7,6 +7,34 @@ from sqlalchemy import create_engine
 import pandas as pd
 import pymysql
 
+# The id number for English in the languages table
+ENGLISH = 17
+
+# which publications to prefere to get an example page number
+# title or the most recent English translation. 
+
+# Within each group, favor newer publications
+def pages_sort(pub_column):
+    if pub_column.name == 'title_id':
+        # Favor the curent title_id above all else, 
+        # which is either the parent title 
+        # or the most recent English translation
+        favor_title_id = lambda id : 1 if id == title_id else 2
+        return pub_column.map(favor_title_id)
+    elif pub_column.name == 'pub_ptype':
+        #Favor hardcovers, then paperback, then trade paperback, 
+        # then ebook, then anything else. 
+        favor_hard_cover = lambda ptype : { 'hc' : 1, 'pb' : 2, 
+            'tp' : 3, 'ebook' : 4}.get(ptype, 5)
+        return pub_column.map(favor_hard_cover)
+    elif pub_column.name == 'p_year' or pub_column.name == 'title_id':
+        # make it negative to affect a descending sort on year or id
+        return -pub_column
+    else:
+        print("Invalid column passed to pages_sort")
+        return pub_column
+
+
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -17,7 +45,7 @@ conn = mysql.connector.connect(
 cur = conn.cursor()
 
 # Get all the novels, novellas, collections, and omnibuses 
-# where an English (code # 17) version exists, 
+# where an English version exists, 
 # and which aren't non-genre, graphic novels, or by an excluded author
 cur.execute("""
     SELECT title_id, title_title, title_synopsis, note_id, series_id, 
@@ -28,7 +56,7 @@ cur.execute("""
         (title_ttype = 'SHORTFICTION' AND title_storylen = 'novella') 
         OR title_ttype IN ('ANTHOLOGY', 'COLLECTION', 'NOVEL', 'OMNIBUS')
     ) 
-    AND title_language = 17
+    AND title_language = %s
     AND title_non_genre != 'Yes' 
     AND title_graphic != 'Yes'
     AND title_id NOT IN (
@@ -37,20 +65,23 @@ cur.execute("""
         WHERE author_id in (4853, 2857, 319407)
     )
     LIMIT 10000
-    ; """)
+    ; """, (ENGLISH,))
 
 
 titles = cur.fetchall()
 
 alchemyEngine = create_engine('mysql+pymysql://root:@127.0.0.1/isfdb')
 alch_conn = alchemyEngine.connect()
-alch_cur = alch_conn.cursor()
 
 i = 0
+debug_id = 10
+# for title_id, title, synopsis_id, note_id, series_id, seriesnum, \
+#     year, ttype, parent_id, rating, seriesnum_2 in [titles[debug_id - 1]]:
 for title_id, title, synopsis_id, note_id, series_id, seriesnum, \
     year, ttype, parent_id, rating, seriesnum_2 in titles:
     
     i += 1
+    print('.', end='', flush=True)
     if i > 10000:
         break
 
@@ -58,30 +89,33 @@ for title_id, title, synopsis_id, note_id, series_id, seriesnum, \
         # This title may be a translation. 
 
         cur.execute("""
-            SELECT lang_name
-            FROM languages
-            WHERE lang_id in (
-                SELECT title_language
-                FROM titles
-                WHERE title_id = %s
-            )
+            SELECT original.title_title, 
+                YEAR(original.title_copyright) as original_year, 
+                original.title_language
+            FROM titles as original
+            JOIN titles as translation
+            ON translation.title_parent = original.title_id
+            WHERE original.title_id = %s
+            LIMIT 1
             """, (parent_id,))
-        original_language = cur.fetchone()[0]
+        original_title, original_year, original_language = cur.fetchone()
+
 
         # If the original language is also English, this is just a variant title.
         # Process this title when the loop is the parent title instead.
-        if original_language == "English":
+        if original_language == ENGLISH:
             continue
 
         # This is an English translation of a foreign language work
         # Get all the English translations
         cur.execute("""
-            SELECT title_id, title_title, YEAR(title_copyright) as year, note_id 
+            SELECT title_id, title_title as translation_title, 
+                YEAR(title_copyright) as translation_year, note_id 
             FROM titles 
-            WHERE title_language = 17 
+            WHERE title_language = %s 
             AND title_parent = %s 
-            ORDER BY year DESC, title_id DESC ;
-            """, (parent_id,))
+            ORDER BY translation_year DESC, title_id DESC ;
+            """, (ENGLISH, parent_id))
         translations = cur.fetchall()
 
         # Wait to process this work if we aren't on the most recent translation
@@ -105,10 +139,19 @@ for title_id, title, synopsis_id, note_id, series_id, seriesnum, \
     # 2157884: Mira's last dance
     # 1475: Nueromancer
     # 41897: call of cthulhlu
+    # 2369460: roadside picnic
 
+    if ttype == "SHORTFICTION":
+        ttype = "NOVELLA"
+
+    # editions, pages, cover_image, isbn = get_pub_fields(title_id, root_id, ttype)
+
+    # TODO: remove fields when I no longer need them for debugging:
+    # title
     all_pubs = pd.read_sql("""
-        SELECT t.title_id, t.title_language, p.pub_id, p.pub_title, YEAR(p.pub_year) as p_year, 
-            p.pub_pages, p.pub_ptype, p.pub_ctype, p.pub_isbn
+        SELECT t.title_id, t.title_language, p.pub_id, 
+            YEAR(p.pub_year) as p_year, p.pub_pages, p.pub_ptype, 
+            p.pub_ctype, p.pub_isbn, p.pub_frontimage
         FROM pubs as p
         JOIN pub_content as c
         ON c.pub_id = p.pub_id
@@ -118,24 +161,20 @@ for title_id, title, synopsis_id, note_id, series_id, seriesnum, \
             OR t.title_parent = %s;""", 
         alch_conn, params=[root_id, root_id])
 
-    if ttype == "SHORTFICTION":
-        ttype = "NOVELLA"
 
     # editions
     if ttype == "NOVEL" or ttype == "NOVELLA":
-        en_books = all_pubs[( (all_pubs.title_language == 17) &
-                        ((all_pubs.pub_ctype == "NOVEL") | 
+        books = all_pubs[( (all_pubs.pub_ctype == "NOVEL") | 
                         (all_pubs.pub_ctype == "CHAPBOOK") |
                         (all_pubs.pub_ctype == "ANTHOLOGY") |
                         (all_pubs.pub_ctype == "COLLECTION") |
-                        (all_pubs.pub_ctype == "OMNIBUS")) )]
+                        (all_pubs.pub_ctype == "OMNIBUS") )]
     else:
         # ttype is ANTHOLOGY, COLLECTION or OMNIBUS and must match 
         # the publication type exactly
-        en_books = all_pubs[( (all_pubs.title_language == 17) &
-                            (all_pubs.pub_ctype == ttype) )]
+        books = all_pubs[( (all_pubs.pub_ctype == ttype) )]
 
-    editions = en_books.shape[0]
+    editions = books[books.title_language == ENGLISH].shape[0]
 
     # If the title was never published in book form, it probably isn't a
     # good book club recommendation, since it is probably hard to find 
@@ -145,10 +184,93 @@ for title_id, title, synopsis_id, note_id, series_id, seriesnum, \
 
     # For pages and pictures, further filter Novels and Novellas
     if ttype == "NOVEL" or ttype == "NOVELLA":
-        en_books = en_books[(en_books.pub_ctype == "NOVEL") | 
-                        (en_books.pub_ctype == "CHAPBOOK")]
+        books = books[(books.pub_ctype == "NOVEL") | 
+                        (books.pub_ctype == "CHAPBOOK")]
+
+    # map all remaining ISBNs to this title_id in the isbn table
+    all_book_isbns = books[( (books.pub_isbn.notnull()) & 
+                            (books.pub_isbn != '') )]\
+                            .pub_isbn.drop_duplicates().to_list()
+    #TODO add isbns to table
+    # for book_isbn in all_book_isbns:
+    #     print(book_isbn)
+
+    # Only deal with English editions from this point on
+    books = books[ books.title_language == ENGLISH]
+
+    # A key method to pass to sort_values to choose the best source for
+    # page number and cover images. Which fields have higher priority, 
+    # or are actually used are determined by the sort_values'by=' keyword
+    def preferred_pubs(pub_column):
+        if pub_column.name == 'title_id':
+            # Favor the curent title_id above all else, 
+            # which is either the parent title 
+            # or the most recent English translation
+            favor_title_id = lambda id : 1 if id == title_id else 2
+            return pub_column.map(favor_title_id)
+        elif pub_column.name == 'pub_ptype':
+            #Favor hardcovers, then paperback, then trade paperback, 
+            # then ebook, then anything else. 
+            favor_hard_cover = lambda ptype : { 'hc' : 1, 'pb' : 2, 
+                'tp' : 3, 'ebook' : 4}.get(ptype, 5)
+            return pub_column.map(favor_hard_cover)
+        elif pub_column.name == 'p_year' or pub_column.name == 'pub_id':
+            # within each group, favor newer publications
+            # make it negative to affect a descending sort on year or id
+            return -pub_column
+        else:
+            print("Invalid column passed to preferred_pubs")
+            return pub_column
+
+    pages = None
+    books = books.sort_values(
+        by=['title_id', 'pub_ptype', 'p_year', 'pub_id'], key=preferred_pubs)
+    for page_str in books.pub_pages:
+        # Might be in a format like: "vii+125+[10]" or "125+[10]"
+        # try the second and then the first positions before giving up
+        for ii in (1,0):
+            try:
+                pages = int(page_str.split("+")[ii])
+                break
+            except AttributeError:
+                pass
+            except IndexError:
+                pass
+            except ValueError:
+                pass
+        if pages:
+            break
 
 
+    # Don't consider binding type cover image for now
+    # TODO: consider removing audio book or ebook covers 
+    # if they cause a quality problem
+    books = books.sort_values(
+        by=['title_id', 'p_year', 'pub_id'], key=preferred_pubs)
+    # only keep external image links if they are from amazon or isfdb
+    allowed_covers = books[(books.pub_frontimage.notnull() ) &
+                        ( ('amazon.com' in str(books.pub_frontimage) ) |
+                        ('amazon.ca' in str(books.pub_frontimage) ) |
+                        ('isfdb.org' in str(books.pub_frontimage) ) )]\
+                        .pub_frontimage.drop_duplicates().to_list()
+
+    if allowed_covers:
+        cover_image = allowed_covers[0]
+        #TODO: insert additional covers into their own table
+        for image in allowed_covers[1:]:
+            pass
+    else:
+        cover_image = None
+
+    preferred_isbns = books[((books.pub_isbn.notnull()) &
+                            (books.pub_isbn != '') &
+                            (books.pub_ptype.str.contains('audio') == False))]\
+                            .pub_isbn.to_list()
+
+    if preferred_isbns:
+        isbn = preferred_isbns[0]
+    else:
+        isbn = None
 
 
     cur.execute("""
@@ -205,10 +327,12 @@ for title_id, title, synopsis_id, note_id, series_id, seriesnum, \
         SELECT title_id
         FROM titles
         WHERE title_parent = %s
-        AND title_language = 17
+        AND title_language = %s
     );
-    """, (title_id,))
+    """, (title_id, ENGLISH))
     title_set = set([r[0] for r in cur.fetchall()]) - set(title)
+    
+    # TODO: filter out alternate titles like: "part x of y"
     if title_set:
         alt_titles_str = "; ".join([title for title in title_set])
 
@@ -238,70 +362,6 @@ for title_id, title, synopsis_id, note_id, series_id, seriesnum, \
             WHERE note_id = %s
             """, (note_id,))
         note = cur.fetchone()[0]
-
-    # page count
-    if ttype in ('NOVEL', 'NOVELLA'):
-        allowed_ctypes = "('NOVEL', 'CHAPBOOK') "
-    else:
-        # ttype is ANTHOLOGY, COLLECTION or OMNIBUS
-        allowed_ctypes = "('" + ttype + "') "
-
-    # Try to get page count of a recent edition. Favor hardcovers, 
-    # then paperback, then trade paperback, then ebook, then anything else
-    sql="""
-    SELECT pub_pages
-    FROM pubs 
-    WHERE pub_id in (
-        SELECT pub_id 
-        FROM pub_content 
-        WHERE title_id = %s
-    ) 
-    AND pub_ctype IN """ \
-    + allowed_ctypes \
-    + """
-    AND pub_pages IS NOT NULL 
-    AND pub_pages != '' 
-    ORDER BY 
-        CASE pub_ptype 
-            WHEN 'hc' THEN 1 
-            WHEN 'pb' THEN 2 
-            WHEN 'tp' THEN 3 
-            when 'ebook' THEN 4
-            ELSE 5
-        END, 
-        pub_year DESC, 
-        pub_id DESC;
-    """
-
-    cur.execute(sql, (title_id,))
-    page_counts = cur.fetchall()
-
-    pages = None
-    for page_count in page_counts:
-        count_str = page_count[0]
-        try:
-            pages = int(count_str)
-            break
-        except ValueError:
-            pass
-
-        # Might be in a format like: "vii+125+[10]" or "125+[10]"
-        for i in (1,0):
-            try:
-                pages = int(count_str.split("+")[i])
-                break
-            except IndexError:
-                pass
-            except ValueError:
-                pass
-        if pages:
-            break
-
-    #TODO: There could be novellas that were never published as 
-    # stand-alone works, but appear in omnibuses. In some cases, it 
-    # could be possible to calculate their page numbers from the omnibus
-    # contents, but it would be a lot of work for a few cases
-
 
     if series_id:
         cur.execute("""
@@ -339,6 +399,10 @@ for title_id, title, synopsis_id, note_id, series_id, seriesnum, \
                 series_str += (series_title + ", ")
             series_str += ("and " + parent_series[-1] + " series.")
 
+
+#TODO: After main loop through titles:
+#    * Populate included_titles table for Omnibuses
+#   
 
 
 cur.close()
